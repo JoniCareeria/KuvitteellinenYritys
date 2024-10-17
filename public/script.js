bd4b6be0bf5b654ace357ae1bc7d0d539fff4ef9
JoniCareeria <Joni.Reijo@student.careeria.fi>
2024-10-17 10:58:16 +0300

    Muutoksia 2commit

--- public/script.js
+++ public/script.js
@@ -3,21 +3,21 @@ function loadPage(page) {
 
 
     switch (page) {
-        case 'home':
+        case 'etusivu':
             content.innerHTML = `
                 <h2>Tervetuloa Yritykseen</h2>
                 <p>Yritys Oy tarjoaa monipuolisia palveluita ja ratkaisuja eri toimialoille. Jos sinulla on yritys autamme sinua. Jos haluat yrityksen me autamme sinua.</p>
                 <p>Yritys Oy on sinunkin yrityksesi</p>
             `;
             break;
-        case 'about':
+        case 'tietoa':
             content.innerHTML = `
                 <h2>Tietoa meistä</h2>
                 <p>Tämä kuvitteellinen Yritys Oy on perustettu vuonna 2024 koulutehtävää varten, ja se tarjoaa innovatiivisia ratkaisuja asiakkailleen.</p>
                 <p>Yritys Oy on sinunkin yrityksesi</p>
             `;
             break;
-        case 'contact':
+        case 'info':
             content.innerHTML = `
                 <h2>Yhteystiedot</h2>
                 <p>Sähköposti: info@yritysoy.fi</p>
@@ -25,18 +25,7 @@ function loadPage(page) {
                 <p>Osoite: Lahdentie 666, 15100 Lahti</p>
             `;
             break;
-        case 'staff1':
-            content.innerHTML = `
-                <h2>Henkilökunta</h2>
-                <ul>
-                    <li><strong>Maija Meikäläinen</strong> - Toimitusjohtaja</li>
-                    <li><strong>Pekka Virtanen</strong> - Myyntipäällikkö</li>
-                    <li><strong>Liisa Lahtinen</strong> - Markkinointikoordinaattori</li>
-                    <li><strong>Mikko Mäkinen</strong> - IT-asiantuntija</li>
-                    <li><strong>Anna Anttila</strong> - Talouspäällikkö</li>
-                </ul>
-            `;
-            break;
+
         case 'staff2':
 
         async function fetchData() {
