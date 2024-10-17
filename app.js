6bffdfc0ea30dcc08f4e51b4e941c9be0d721623
JoniCareeria <Joni.Reijo@student.careeria.fi>
2024-10-17 11:13:36 +0300

    portti muutos julkaisuun

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3030;
 
 const henkilokunta = require('./henkilokunta.json')
 
@@ -20,5 +20,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
